1e1713a06add591ae26b77f77c0001a43ccdead0
marcus <marcus@kali.marcus>
2022-08-21 13:47:45 -0400

    here we go

---
+++ js/main.js
@@ -0,0 +1,56 @@
+let k = Number(prompt('Sonni kirting:'));  //1-masala
+let n = Number (prompt('Necha marta takrorlanishi kerakligini kiriting:'))
+
+for (let i = 1; i <= n; i++) {
+    console.log(k);   
+}
+
+
+
+// let a = Number(prompt('a sonini kiriting:'));  // 2-masala
+// let b = Number(prompt('b sonini kiriting:'));
+// let c = ('Barcha butun sonlar soni:');
+
+// if (a<b) {
+//     console.log(c, b - a + 1)
+// } else {
+//     console.log("Siz noto'g'ri son kiritdingiz, boshidan urinib ko'ring!")
+// }
+
+// for (let i = a; i <= b; i++) {
+//     console.log(i);
+// }
+
+
+
+// let a = Number(prompt('a(katta) sonini kiriting:'));  // 3-masala
+// let b = Number(prompt('b(kichik) sonini kiriting:'));
+// let c = ('Barcha butun sonlar soni:');
+
+// if (a>b) {
+//     console.log(c, a - b - 1)
+// } else {
+//     console.log("Siz noto'g'ri son kiritdingiz, boshidan urinib ko'ring!")
+// }
+
+// for (let i = a-1; i > b; i--) {
+//     console.log(i);
+// }
+
+
+
+// const kg = 5000; // 4-masala
+// let n = 10;
+
+// for (let i = 1; i <= n;  i++) {
+//     console.log(i +"kg", i * kg)
+// }
+
+
+
+// const kg = 5000; // 5-masala
+// let n = 10;
+
+// for (let i = 1; i <= n;  i++) {
+//     console.log(i/10 +"kg", i/10 * kg)
+// }
